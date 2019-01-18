1bb31430abefe1c91271e8f763dd5819ccac8c13
Matthew Osborn <matthew@osbornm.com>
2019-01-17 21:24:52 -0600

    Styleguide adding copy to colorSwatches

--- src/Styleguide/ColorSwatch.js
+++ src/Styleguide/ColorSwatch.js
@@ -1,43 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class ColorSwatch extends React.PureComponent {
-  render() {
-    const {
-      name,
-      value,
-      width,
-      height
-    } = this.props;
-    return (
-      <div style={{
-          display: 'inline-block',
-          marginBottom: '32px'
-        }}
-      >
-        <div
-          style={{
-            width: `${width}px`,
-            height: `${height}px`,
-            backgroundColor: value,
-            display: 'inline-block'
-          }}
-        />
-        <div>{name}</div>
-        <div>{value}</div>
-      </div>
-    );
-  }
-}
+const copyToClipboard = (str) => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
+
+const ColorSwatch = ({
+  name,
+  value,
+  width = 200,
+  height = 100
+}) => (
+  <div style={{
+    display: 'inline-block',
+    marginBottom: '32px'
+  }}
+  >
+    <div
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        backgroundColor: value,
+        display: 'inline-block'
+      }}
+    />
+    <div title="click to copy" onClick={() => { copyToClipboard(name); }} style={{ fontWeight: 600 }}>{name}</div>
+    <div title="click to copy" onClick={() => { copyToClipboard(value); }}>{value}</div>
+  </div>
+);
 
 ColorSwatch.propTypes = {
   name: PropTypes.string,
-  value: PropTypes.string,
+  value: PropTypes.string.isRequired,
   width: PropTypes.number,
   height: PropTypes.number
 };
 
-ColorSwatch.defaultProps = {
-  width: 200,
-  height: 100
-};
+export default ColorSwatch;
