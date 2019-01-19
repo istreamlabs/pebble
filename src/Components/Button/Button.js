675938876984329ba7218035e12c782fae1a1c47
Nathan Young <nyoung@istreamplanet.com>
2019-01-19 10:32:49 -0800

    fix lint

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -8,6 +8,7 @@ import './Button.scss';
 // eslint-disable-next-line react/button-has-type
 const Button = (
   {
+    id,
     children,
     className,
     size,
@@ -27,13 +28,12 @@ const Button = (
     ariaPressed,
   }
 ) => {
-
   const isDisabled = loading || disabled;
 
   const classes = classNames('btn', className, {
     'btn--loading': loading,
-    [`btn-lg`]: size === 'large',
-    [`btn-sm`]: size === 'small',
+    'btn-lg': size === 'large',
+    'btn-sm': size === 'small',
     'btn--primary-danger': primary && danger,
     'btn--primary': primary,
     'btn--danger': danger,
@@ -47,7 +47,14 @@ const Button = (
   // TO DO: replace with actual Icon component spinner
   const spinnerSVGMarkup = loading ? (
     <span className="btn-loading-spinner">
-      <svg viewBox="0 0 16 16" width="16" height="16"><title>circle 04</title><g fill="#555555"><g> <path fill="currentColor" d="M8,16c-1.644043,0-3.2236328-0.4951172-4.5683594-1.4321289 c-0.453125-0.3154297-0.5644531-0.9389648-0.2485352-1.3920898c0.3164062-0.4521484,0.9399414-0.5644531,1.3920898-0.2485352 C5.5825195,13.6289062,6.7670898,14,8,14c3.3085938,0,6-2.6914062,6-6s-2.6914062-6-6-6S2,4.6914062,2,8 c0,0.5844727,0.0834961,1.1616211,0.2490234,1.7158203c0.1577148,0.5292969-0.1435547,1.0864258-0.6728516,1.2441406 c-0.5283203,0.1572266-1.0864258-0.1435547-1.2441406-0.6728516C0.1118164,9.5473633,0,8.777832,0,8c0-4.4111328,3.5888672-8,8-8 s8,3.5888672,8,8S12.4111328,16,8,16z" transform="rotate(106.50399999320507 8 8)"></path> </g></g></svg>
+      <svg viewBox="0 0 16 16" width="16" height="16">
+        <title>circle 04</title>
+        <g fill="#555555">
+          <g>
+            <path fill="currentColor" d="M8,16c-1.644043,0-3.2236328-0.4951172-4.5683594-1.4321289 c-0.453125-0.3154297-0.5644531-0.9389648-0.2485352-1.3920898c0.3164062-0.4521484,0.9399414-0.5644531,1.3920898-0.2485352 C5.5825195,13.6289062,6.7670898,14,8,14c3.3085938,0,6-2.6914062,6-6s-2.6914062-6-6-6S2,4.6914062,2,8 c0,0.5844727,0.0834961,1.1616211,0.2490234,1.7158203c0.1577148,0.5292969-0.1435547,1.0864258-0.6728516,1.2441406 c-0.5283203,0.1572266-1.0864258-0.1435547-1.2441406-0.6728516C0.1118164,9.5473633,0,8.777832,0,8c0-4.4111328,3.5888672-8,8-8 s8,3.5888672,8,8S12.4111328,16,8,16z" transform="rotate(106.50399999320507 8 8)" />
+          </g>
+        </g>
+      </svg>
     </span>
   ) : null;
 
@@ -60,24 +67,27 @@ const Button = (
   );
 
   return (
-    <button 
+    /* eslint-disable react/button-has-type */
+    <button
+      disabled={isDisabled}
+      id={id}
       type={type}
       className={classes}
       onClick={onClick}
       onFocus={onFocus}
       onBlur={onBlur}
-      disabled={isDisabled}
       tabIndex={tabIndex}
       aria-label={accessibilityLabel}
       aria-controls={ariaControls}
       aria-expanded={ariaExpanded}
       aria-pressed={ariaPressed}
       aria-busy={loading ? true : undefined}
-  >
+    >
       {content}
     </button>
-  )
-}
+    /* eslint-enable react/button-has-type */
+  );
+};
 
 Button.defaultProps = {
   type: 'button',
@@ -115,7 +125,7 @@ Button.propTypes = {
    * Disables the button, making it inoperable
    */
   disabled: PropTypes.bool,
-  /** 
+  /**
    * Replaces button contents with a spinner icon while a background action is being performed
    */
   loading: PropTypes.bool,
@@ -128,8 +138,9 @@ Button.propTypes = {
    */
   primary: PropTypes.bool,
   /**
-   * Optional prop to specify the tabIndex of the Button
+   * Indicate pressing the button will perform a destructive action
    */
+  danger: PropTypes.bool,
   /**
    * Specify the button take up the full width of its parent container
    */
@@ -142,15 +153,15 @@ Button.propTypes = {
    * Visually hidden text for screen readers
    */
   accessibilityLabel: PropTypes.string,
-  /** 
+  /**
    * Id of the element the button controls
   */
   ariaControls: PropTypes.string,
-  /** 
+  /**
    * Tells screen reader the controlled element is expanded
   */
   ariaExpanded: PropTypes.bool,
-  /** 
+  /**
    * Tells screen reader the element is pressed
    */
   ariaPressed: PropTypes.bool,
@@ -158,7 +169,7 @@ Button.propTypes = {
    * Callback when button is pressed
    */
   onClick: PropTypes.func,
-  /** 
+  /**
    * Callback when button receives focus
    */
   onFocus: PropTypes.func,
