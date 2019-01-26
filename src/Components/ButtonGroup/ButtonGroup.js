c6ad35a8f71eeeef35efb5f4750593b6272c4b9a
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2019-01-26 09:05:38 -0800

    ButtonGroup component (#5)
    * apply correct border color
    
    * button group component
    
    * fix lint and tests
    
    * cleanup

---
+++ src/Components/ButtonGroup/ButtonGroup.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import './ButtonGroup.scss';
+
+const ButtonGroup = (
+  {
+    children,
+    className,
+    fullWidth,
+    size,
+    toolbar,
+  }
+) => {
+  const classes = classNames(className, {
+    'btn-group': !toolbar,
+    'btn-toolbar': toolbar,
+    'btn-group-fullwidth': fullWidth,
+  });
+
+  return (
+    <div className={classes}>
+      {React.Children.map(children, child => (
+        <div className={toolbar ? 'btn-toolbar-item' : 'btn-group-item'}>
+          {size ? React.cloneElement(child, { size }) : child}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+ButtonGroup.propTypes = {
+  /**
+   * Additional ClassNames to add to button group
+   */
+  className: PropTypes.string,
+  /**
+   * Contents of the group
+   */
+  children: PropTypes.node.isRequired,
+  /**
+   * Takes up the full width of its parent container
+   */
+  fullWidth: PropTypes.bool,
+  /**
+   * Changes the size of all buttons in the group
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Removes space between buttons and combines them into a button bar
+   */
+  toolbar: PropTypes.bool,
+};
+
+export default ButtonGroup;
---
+++ src/Components/ButtonGroup/ButtonGroup.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Button from '../Button/Button';
+import ButtonGroup from './ButtonGroup';
+
+describe('Button', () => {
+  it('renders without crashing', () => {
+    expect(() => { shallow(<ButtonGroup>hello</ButtonGroup>); }).not.toThrow();
+  });
+
+  describe('children', () => {
+    it('renders child elements', () => {
+      const buttonGroup = shallow(
+        <ButtonGroup>
+          <Button>button</Button>
+          <Button primary>save</Button>
+        </ButtonGroup>
+      );
+      expect(buttonGroup.find(Button)).toHaveLength(2);
+    });
+  });
+
+  describe('className', () => {
+    it('passes in class name', () => {
+      const buttonGroup = shallow(<ButtonGroup className="my-class">foo</ButtonGroup>);
+      expect(buttonGroup.html()).toContain('btn-group');
+      expect(buttonGroup.html()).toContain('my-class');
+    });
+  });
+
+  it('applies fullwidth', () => {
+    const buttonGroup = shallow(<ButtonGroup fullWidth>foo</ButtonGroup>);
+    expect(buttonGroup.html()).toContain('btn-group-fullwidth');
+  });
+
+  it('applies toolbar styles', () => {
+    const buttonGroup = shallow(
+      <ButtonGroup toolbar>
+        <Button>button 1</Button>
+        <Button>button 2</Button>
+      </ButtonGroup>
+    );
+    expect(buttonGroup.find('div.btn-toolbar-item')).toHaveLength(2);
+  });
+
+  it('applies size to child button', () => {
+    const buttonGroup = shallow(
+      <ButtonGroup size="large">
+        <Button>button 1</Button>
+      </ButtonGroup>
+    );
+    expect(buttonGroup.find(Button).prop('size')).toBe('large');
+  });
+});
