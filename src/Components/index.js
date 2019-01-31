609511192847cedd3e77ceff5fb6b1014a850cfc
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2019-01-31 13:36:12 -0800

    Heading component (#7)
    * heading component
    
    * pr feedback
    
    * make Headings responsive by default
    
    * remove map

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './Button/Button';
 export { default as ButtonGroup } from './ButtonGroup/ButtonGroup';
+export { default as Heading } from './Heading/Heading';
 export { default as LinkButton } from './LinkButton/LinkButton';
