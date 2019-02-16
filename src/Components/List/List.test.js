90b9ffcb7717a5fd20a236c18dfdd465de2d4475
Nathan Young <nyoung@istreamplanet.com>
2019-02-15 16:25:21 -0800

    List component

---
+++ src/Components/List/List.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import List from './List';
+
+describe('List', () => {
+  it('renders without crashing', () => {
+    expect(() => { shallow(<List><li>item</li></List>); }).not.toThrow();
+  });
+
+  it('renders children', () => {
+    const wrapper = shallow(<List><li>item</li></List>);
+    expect(wrapper.find('li').text()).toBe('item');
+  });
+
+  it('allows for custom classes', () => {
+    const wrapper = shallow(<List className="myClass"><li>item</li></List>);
+    expect(wrapper.find('ul').prop('className')).toContain('myClass');
+  });
+
+  it('renders ordered list', () => {
+    const wrapper = shallow(<List ordered><li>item</li></List>);
+    expect(wrapper.find('ol').length).toBe(1);
+  });
+
+  it('sets the text size', () => {
+    const wrapper = mount(<List size="1">test</List>);
+    expect(wrapper.find('.text-container').prop('className')).toContain('fs-1');
+  });
+});
---
+++ src/Components/List/Readme.md
@@ -0,0 +1,32 @@
+## Examples
+
+### Unordered List
+```js
+<List>
+  <ListItem>Cartoon Network</ListItem>
+  <ListItem>CNN</ListItem>
+  <ListItem>TBS</ListItem>
+  <ListItem>TBS</ListItem>
+</List>
+```
+
+### Ordered List
+```js
+<List ordered>
+  <ListItem>Cartoon Network</ListItem>
+  <ListItem>CNN</ListItem>
+  <ListItem>TBS</ListItem>
+  <ListItem>TBS</ListItem>
+</List>
+```
+
+
+### Size
+```js
+<List size="3">
+  <ListItem>Cartoon Network</ListItem>
+  <ListItem>CNN</ListItem>
+  <ListItem>TBS</ListItem>
+  <ListItem>TBS</ListItem>
+</List>
+```
