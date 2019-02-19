6d6cef0c32a50ae1b8fc80c76ef2e8762adeafff
Matthew Osborn <matthew@osbornm.com>
2019-02-19 10:05:11 -0600

    add tests for proptype validator

---
+++ src/Utils/BoolRequiresOtherProp.test.js
@@ -0,0 +1,28 @@
+import boolRequiresOtherProp from './BoolRequiresOtherProp';
+
+describe('Util > BoolRequiresOtherProp', () => {
+  it('should do nothing if first prop undefined', () => {
+    const func = boolRequiresOtherProp('myOtherProp');
+    const error = func({}, 'myProp', 'myComponent');
+    expect(error).toBeUndefined();
+  });
+
+  it('should do nothing if first prop not bool', () => {
+    const func = boolRequiresOtherProp('myOtherProp');
+    const error = func({ myProp: 123 }, 'myProp', 'myComponent');
+    expect(error).toBeUndefined();
+  });
+
+  it('should do nothing if prop is a boolean and other prop defined', () => {
+    const func = boolRequiresOtherProp('myOtherProp');
+    const error = func({ myProp: true, myOtherProp: 'abc' }, 'myProp', 'myComponent');
+    expect(error).toBeUndefined();
+  });
+
+  it('throw if prop is a boolean and other prop not defined ', () => {
+    const func = boolRequiresOtherProp('myOtherProp');
+    const error = func({ myProp: true }, 'myProp', 'myComponent');
+    expect(error).toBeDefined();
+    expect(error.message).toEqual("Please provide a 'myOtherProp' in order to use 'myProp' in <myComponent>");
+  });
+});
