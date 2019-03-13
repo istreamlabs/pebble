ea438b9d3ab640e4edd8c4ce0d56e911c7c00109
Nathan Young <nyoung@istreamplanet.com>
2019-03-13 15:08:59 -0700

    story changes

--- src/Components/Card/stories.js
+++ src/Components/Card/stories.js
@@ -4,12 +4,43 @@ import '../../Styles/foundation.scss';
 
 import Card from './Card';
 
+import Button from '../Button/Button';
+import ButtonGroup from '../ButtonGroup/ButtonGroup';
+
 storiesOf('Card', module)
   .add('all', () => (
     <>
       <Card>
         <div>First Child</div>
         <div>Second Child</div>
+      </Card>
+      <Card title="Basic Card" sectioned>
+        <div>First Child</div>
+        <div>Second Child</div>
+      </Card>
+      <Card title="Packages" sectioned>
+        <div>Platinum Package</div>
+        <div>Gold Package</div>
+        <div>Silver Package</div>
+        <div>Bronze Package</div>
+      </Card>
+      <Card
+        title="Packages"
+        sectioned
+        headerActions={(
+          <ButtonGroup>
+            <Button icon="add-circle" primary>add</Button>
+            <Button>select</Button>
+          </ButtonGroup>
+        )}
+      >
+        <p>Platinum Package</p>
+        <p>Gold Package</p>
+        <p>Silver Package</p>
+        <p>Bronze Package</p>
+      </Card>
+      <Card title="Muted Card" muted sectioned>
+        <p>Use a subdued card to display related content with less visual emphasis.</p>
       </Card>
     </>
   ));
--- src/Components/DropdownMenu/stories.js
+++ src/Components/DropdownMenu/stories.js
@@ -5,22 +5,42 @@ import '../../Styles/foundation.scss';
 import DropdownMenu from './DropdownMenu';
 import DropdownItemGroup from './DropdownItemGroup';
 import DropdownItem from './DropdownItem';
+import DropdownMenuSeparator from './DropdownMenuSeparator';
+
+import Button from '../Button/Button';
+import ButtonGroup from '../ButtonGroup/ButtonGroup';
 
 storiesOf('DropdownMenu', module)
   .add('all', () => (
     <>
-      <DropdownMenu toggle="Dropdown with Item Groups">
-        <DropdownItemGroup title="Transcoder">
-          <DropdownItem icon="play">start</DropdownItem>
-          <DropdownItem icon="stop">stop</DropdownItem>
-          <DropdownItem icon="refresh">restart</DropdownItem>
-          <DropdownItem disabled>disabled</DropdownItem>
-        </DropdownItemGroup>
-        <DropdownItemGroup title="Components">
-          <DropdownItem href="#/Components/Button">Button</DropdownItem>
-          <DropdownItem href="#/Components/Frame">Frame</DropdownItem>
-          <DropdownItem href="#/Components/Text">Text</DropdownItem>
-        </DropdownItemGroup>
-      </DropdownMenu>
+      <div>
+        <DropdownMenu open toggle="Dropdown" overlayClassName="w5">
+          <DropdownItem icon="add-bold">default button</DropdownItem>
+          <DropdownItem href="/">link</DropdownItem>
+          <DropdownMenuSeparator />
+          <DropdownItem danger icon="remove-circle">danger button</DropdownItem>
+        </DropdownMenu>
+      </div>
+      <div>
+        <DropdownMenu toggle="Dropdown with Item Groups">
+          <DropdownItemGroup title="Transcoder">
+            <DropdownItem icon="play">start</DropdownItem>
+            <DropdownItem icon="stop">stop</DropdownItem>
+            <DropdownItem icon="refresh">restart</DropdownItem>
+            <DropdownItem disabled>disabled</DropdownItem>
+          </DropdownItemGroup>
+          <DropdownItemGroup title="Components">
+            <DropdownItem href="#/Components/Button">Button</DropdownItem>
+            <DropdownItem href="#/Components/Frame">Frame</DropdownItem>
+            <DropdownItem href="#/Components/Text">Text</DropdownItem>
+          </DropdownItemGroup>
+        </DropdownMenu>
+      </div>
+      <div>
+        <DropdownMenu toggle="Disabled Dropdown" disabled>
+          <DropdownItem>disabled</DropdownItem>
+          <DropdownItem>disabled</DropdownItem>
+        </DropdownMenu>
+      </div>
     </>
   ));
