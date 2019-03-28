280611ec5c1bf8b0c48090cc4969a3a495ff6ab4
Nathan Young <nyoung@istreamplanet.com>
2019-03-28 16:21:22 -0700

    show code samples

--- styleguide.config.js
+++ styleguide.config.js
@@ -200,7 +200,7 @@ module.exports = {
       name: 'Components',
       content: 'src/Styleguide/Docs/Components.md',
       components: 'src/Components/*/*.js',
-      exampleMode: 'collapse',
+      exampleMode: 'expand',
       usageMode: 'expand',
       sectionDepth: 2,
     }
