fb51135f3f93946dc16cdc71dc4423d13ff041e6
Nathan Young <nyoung@istreamplanet.com>
2019-05-01 11:26:46 -0700

    update story

--- src/Components/Breadcrumbs/stories.js
+++ src/Components/Breadcrumbs/stories.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { BrowserRouter as DemoBrowserRouter } from 'react-router-dom';
 
+import Block from '../Block/Block';
 import Breadcrumbs from './Breadcrumbs';
 import Button from '../Button/Button';
 import Link from '../Link/Link';
@@ -18,7 +19,20 @@ storiesOf('Breadcrumbs', module)
           <DropdownItem>Level 1a</DropdownItem>
           <DropdownItem>Level 1b</DropdownItem>
         </DropdownMenu>
-        <Link href="#">Level 2</Link>
+        <Block alignItems="center" itemSpacing="2">
+          <div>
+            <Link href="/">Level 2</Link>
+          </div>
+          <DropdownMenu
+            toggle={
+              <Button plain accessibilityLabel="" icon="arrow-small-down" />
+            }
+          >
+            <DropdownItem href="#">Level 2a</DropdownItem>
+            <DropdownItem href="#">Level 2b</DropdownItem>
+            <DropdownItem href="#">Level 2c</DropdownItem>
+          </DropdownMenu>
+        </Block>
         <Link href="#">Level 3</Link>
       </Breadcrumbs>
     </DemoBrowserRouter>
