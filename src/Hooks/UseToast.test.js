17ea680270b162ea738ab3e881f8622d4b57210e
Matthew Osborn <matthew@osbornm.com>
2019-06-05 10:13:16 -0500

    test wip

--- src/Hooks/UseToast.test.js
+++ src/Hooks/UseToast.test.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { ToastAlert, useToast } from './UseToast';
+import { ToastAlert, default as useToast } from './UseToast';
+import Alert from '../Components/Alert/Alert';
+import { toast } from 'react-toastify';
+
+jest.mock('react-toastify');
 
 describe('ToastAlert', () => {
   it('renders without crashing', () => {
     expect(() => { shallow(<ToastAlert title="Product updated" />); }).not.toThrow();
   });
-
-  describe('type', () => {
-    it('defaults to default', () => {
-      expect(ToastAlert.defaultProps.type).toBe('default');
-    });
+  it('defaults', () => {
+    expect(ToastAlert.defaultProps.type).toBe('default');
+    expect(ToastAlert.defaultProps.position).toBe('bottom');
+  });
+  it('', () => {
+    const wrapper = shallow(<ToastAlert type="warn" title="Product updated" />);
+    // renders single element
+    expect(wrapper.children().length).toEqual(0);
+    const alert = wrapper.find(Alert);
+    expect(alert).toBeDefined();
+    expect(alert.prop('type')).toEqual('warn');
+    expect(alert.prop('className')).toEqual('mb-0');
+    expect(alert.prop('title')).toEqual('Product updated');
   });
 });
 
 describe('Hooks > UseToast', () => {
   it('calls react-notify toast with the correct options', () => {
-    const instance = useToast({
+    const hook = useToast()({
       title: 'New Features Added',
       type: 'info',
       autoClose: 1000,
     });
+    console.log(hook);
+    expect(toast).toHaveBeenCalled();
   });
 });
