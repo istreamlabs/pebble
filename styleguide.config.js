3c1436e502ee40d7fa3a382e525123e9c2e2e470
Nathan Young <nyoung@istreamplanet.com>
2019-06-10 17:22:10 -0700

    correct Hooks path in documentation

--- styleguide.config.js
+++ styleguide.config.js
@@ -21,7 +21,7 @@ module.exports = {
   getComponentPathLine(componentPath) {
     const name = path.basename(componentPath, '.js');
     if (name === 'Toast') {
-      return `import { use${name} } from '@istreamplanet/pebble`;
+      return `import { use${name} } from '@istreamplanet/pebble/dist/Hooks/`;
     }
     return `import { ${name} } from '@istreamplanet/pebble';`;
   },
