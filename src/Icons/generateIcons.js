4721c3f066f83d2516c7a06ee0cbf602812fd8a5
Nathan Young <nathanyoung@users.noreply.github.com>
2019-07-23 07:15:47 -0700

    generate Icon story

--- src/Icons/generateIcons.js
+++ src/Icons/generateIcons.js
@@ -8,82 +8,119 @@ const svgtojsx = require('svg-to-jsx');
 const readFile = promisify(fs.readFile);
 const glob = promisify(Glob.glob);
 const svgo = new SVGO({
-  plugins: [{
-    cleanupAttrs: true,
-  }, {
-    removeDoctype: true,
-  }, {
-    removeXMLProcInst: true,
-  }, {
-    removeComments: true,
-  }, {
-    removeMetadata: true,
-  }, {
-    removeTitle: true,
-  }, {
-    removeDesc: true,
-  }, {
-    removeUselessDefs: true,
-  }, {
-    removeEditorsNSData: true,
-  }, {
-    removeEmptyAttrs: true,
-  }, {
-    removeHiddenElems: true,
-  }, {
-    removeEmptyText: true,
-  }, {
-    removeEmptyContainers: true,
-  }, {
-    removeViewBox: false,
-  }, {
-    cleanupEnableBackground: true,
-  }, {
-    convertStyleToAttrs: true,
-  }, {
-    convertColors: { currentColor: true },
-  }, {
-    convertPathData: true,
-  }, {
-    convertTransform: true,
-  }, {
-    removeUnknownsAndDefaults: true,
-  }, {
-    removeNonInheritableGroupAttrs: true,
-  }, {
-    removeUselessStrokeAndFill: true,
-  }, {
-    removeUnusedNS: true,
-  }, {
-    cleanupIDs: true,
-  }, {
-    cleanupNumericValues: true,
-  }, {
-    moveElemsAttrsToGroup: true,
-  }, {
-    moveGroupAttrsToElems: true,
-  }, {
-    collapseGroups: true,
-  }, {
-    removeRasterImages: false,
-  }, {
-    mergePaths: true,
-  }, {
-    convertShapeToPath: true,
-  }, {
-    sortAttrs: true,
-  }, {
-    removeDimensions: true,
-  }, {
-    removeAttrs: { attrs: '(class|data-.*)' },
-  }]
+  plugins: [
+    {
+      cleanupAttrs: true,
+    },
+    {
+      removeDoctype: true,
+    },
+    {
+      removeXMLProcInst: true,
+    },
+    {
+      removeComments: true,
+    },
+    {
+      removeMetadata: true,
+    },
+    {
+      removeTitle: true,
+    },
+    {
+      removeDesc: true,
+    },
+    {
+      removeUselessDefs: true,
+    },
+    {
+      removeEditorsNSData: true,
+    },
+    {
+      removeEmptyAttrs: true,
+    },
+    {
+      removeHiddenElems: true,
+    },
+    {
+      removeEmptyText: true,
+    },
+    {
+      removeEmptyContainers: true,
+    },
+    {
+      removeViewBox: false,
+    },
+    {
+      cleanupEnableBackground: true,
+    },
+    {
+      convertStyleToAttrs: true,
+    },
+    {
+      convertColors: { currentColor: true },
+    },
+    {
+      convertPathData: true,
+    },
+    {
+      convertTransform: true,
+    },
+    {
+      removeUnknownsAndDefaults: true,
+    },
+    {
+      removeNonInheritableGroupAttrs: true,
+    },
+    {
+      removeUselessStrokeAndFill: true,
+    },
+    {
+      removeUnusedNS: true,
+    },
+    {
+      cleanupIDs: true,
+    },
+    {
+      cleanupNumericValues: true,
+    },
+    {
+      moveElemsAttrsToGroup: true,
+    },
+    {
+      moveGroupAttrsToElems: true,
+    },
+    {
+      collapseGroups: true,
+    },
+    {
+      removeRasterImages: false,
+    },
+    {
+      mergePaths: true,
+    },
+    {
+      convertShapeToPath: true,
+    },
+    {
+      sortAttrs: true,
+    },
+    {
+      removeDimensions: true,
+    },
+    {
+      removeAttrs: { attrs: '(class|data-.*)' },
+    },
+  ],
 });
 
 // Get the contents of the optimized SVG
 // by trimming leading and tailing <svg> tags
-const getSVGContent = source => source.slice(source.indexOf('>') + 1).slice(0, -6);
+const getSVGContent = source =>
+  source.slice(source.indexOf('>') + 1).slice(0, -6);
 
-const getName = filepath => path.basename(filepath, path.extname(filepath));
+const getName = filepath =>
+  path.basename(filepath, path.extname(filepath));
 
 async function main() {
   try {
@@ -91,13 +128,15 @@ async function main() {
     const iconExports = [];
     const examples = [];
 
-    const promises = originalIcons.map(async (iconFilePath) => {
+    const promises = originalIcons.map(async iconFilePath => {
       try {
         const data = await readFile(iconFilePath);
         const optimizeData = await svgo.optimize(data);
         const optimizedJSX = await svgtojsx(optimizeData.data);
         const iconName = getName(iconFilePath).toLocaleLowerCase();
-        const exportRow = `'${iconName}': ${getSVGContent(optimizedJSX)},`;
+        const exportRow = `'${iconName}': ${getSVGContent(
+          optimizedJSX,
+        )},`;
         const example = `<div className="__icon"><Icon name="${iconName}" accessibilityLabel="${iconName}" /><div>${iconName}</div></div>`;
         iconExports.push(exportRow);
         examples.push(example);
@@ -109,6 +148,24 @@ async function main() {
     });
     await Promise.all(promises);
 
+    const storiesFileToWrite = `// THIS FILE IS AUTO GENERATED
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import '../../Styles/foundation.scss';
+import Icon from './Icon';
+storiesOf('Icon', module).add('all', () => (
+  <>
+  <div className="styleguide__icons">
+${examples.join('\t\n')}
+  </div>
+  </>
+));
+    `;
+    await fs.outputFile(
+      './src/Components/Icon/stories.js',
+      storiesFileToWrite,
+    );
+
     const fileToWrite = `// THIS FILE IS AUTO GENERATED
 import React from 'react';
 /* eslint-disable */
@@ -117,8 +174,10 @@ ${iconExports.join('\t\n')}
 };
 /* eslint-enable */
 `;
-    await fs.outputFile('./src/Components/Icon/Icons.js', fileToWrite);
-
+    await fs.outputFile(
+      './src/Components/Icon/Icons.js',
+      fileToWrite,
+    );
 
     const exampleFileToWrite = `
   [//]: # (THIS FILE IS AUTO GENERATED)
@@ -131,7 +190,10 @@ ${examples.join('\t\n')}
 \`\`\`
 `;
 
-    await fs.outputFile('./src/Components/Icon/IconExamples.md', exampleFileToWrite);
+    await fs.outputFile(
+      './src/Components/Icon/IconExamples.md',
+      exampleFileToWrite,
+    );
   } catch (e) {
     /* eslint-disable */
     console.error('error building', e);
