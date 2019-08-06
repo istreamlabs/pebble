d247e017b6ad0795fb8338514dbc2c8306889706
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2019-08-06 10:49:41 -0700

    feat: Ref forwarding for Tooltip (#168)
    * refactor: use forwardRef so that they can be used as Tooltip children
    
    * fix Link

--- src/Components/LinkButton/LinkButton.test.js
+++ src/Components/LinkButton/LinkButton.test.js
@@ -67,9 +67,12 @@ describe('LinkButton', () => {
           link button
         </LinkButton>,
       );
-      expect(button.find('ForwardRef').prop('name')).toBe(
-        'add-circle',
-      );
+      expect(
+        button
+          .find('ForwardRef')
+          .at(1)
+          .prop('name'),
+      ).toBe('add-circle');
     });
     it('sets the correct icon size', () => {
       const button = shallow(
@@ -77,7 +80,12 @@ describe('LinkButton', () => {
           link button
         </LinkButton>,
       );
-      expect(button.find('ForwardRef').prop('size')).toBe(20);
+      expect(
+        button
+          .find('ForwardRef')
+          .at(1)
+          .prop('size'),
+      ).toBe(20);
     });
     it('it places icon to left of button text', () => {
       const button = shallow(
--- src/Components/Tooltip/stories.js
+++ src/Components/Tooltip/stories.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { BrowserRouter as DemoBrowserRouter } from 'react-router-dom';
 
 import Tooltip from './Tooltip';
 import Button from '../Button/Button';
-import Icon from '../Icon/Icon';
 import ButtonGroup from '../ButtonGroup/ButtonGroup';
+import Icon from '../Icon/Icon';
+import Link from '../Link/Link';
+import LinkButton from '../LinkButton/LinkButton';
+import Text from '../Text/Text';
 
 function PlacementExample() {
   return (
@@ -42,12 +46,56 @@ storiesOf('Tooltip', module)
       string
     </Tooltip>
   ))
-  .add('icon', () => (
+  .add('Icon', () => (
     <Tooltip
       active
       content="i'm the content of a tooltip. i'm the content of a tooltip"
     >
       <Icon name="info-circle" />
     </Tooltip>
+  ))
+  .add('Text', () => (
+    <Tooltip
+      active
+      content="i'm the content of a tooltip. i'm the content of a tooltip"
+    >
+      <Text bold>Text component</Text>
+    </Tooltip>
+  ))
+  .add('Button', () => (
+    <Tooltip
+      active
+      content="i'm the content of a tooltip. i'm the content of a tooltip"
+    >
+      <Button primary>Button component</Button>
+    </Tooltip>
+  ))
+  .add('Link', () => (
+    <DemoBrowserRouter>
+      <div>
+        <Tooltip
+          active
+          content="i'm the content of a tooltip. i'm the content of a tooltip"
+        >
+          <Link external href="/#">
+            Link (external) component
+          </Link>
+        </Tooltip>
+      </div>
+      <Tooltip
+        active
+        placement="right"
+        content="i'm the content of a tooltip. i'm the content of a tooltip"
+      >
+        <Link href="/#">Link component</Link>
+      </Tooltip>
+    </DemoBrowserRouter>
+  ))
+  .add('LinkButton', () => (
+    <DemoBrowserRouter>
+      <Tooltip content="i'm the content of a tooltip. i'm the content of a tooltip">
+        <LinkButton href="/#">LinkButton component</LinkButton>
+      </Tooltip>
+    </DemoBrowserRouter>
   ))
   .add('placement', () => <PlacementExample />);
