16ffa136c3ce8b70d5f0326b588576f5f76d6da0
Nathan Young <nathanyoung@users.noreply.github.com>
2019-08-08 14:10:12 -0700

    border options in more components

--- src/Components/Table/Components/TableHeader.js
+++ src/Components/Table/Components/TableHeader.js
@@ -3,10 +3,31 @@ import PropTypes from 'prop-types';
 
 import { useMobileLayout } from '../../../Hooks';
 
+import { borderType } from '../../../Types';
+
 import Block from '../../Block/Block';
 import Heading from '../../Heading/Heading';
 
 const propTypes = {
+  /**
+   *
+   * Apply a `solid 1px neutral-300` border to a specific side by passing one of the following strings:
+   *
+   * 'all', 'vertical', 'horizontal', 'top', 'right', 'bottom', 'left'
+   *
+   * or the pass the shape:
+   * ```
+   * {
+   *   color: "neutral-300",
+   *   side: "all",
+   *   size: "1px",
+   *   style: "solid",
+   * }
+   * ```
+   *
+   * `color` [color identifier](/#/Styles/Color) of the border color
+   */
+  border: borderType,
   /**
    * Contents of the component
    */
@@ -23,7 +44,11 @@ const propTypes = {
  * ---
  */
 
-function TableHeader({ children, mobileLabel }) {
+const defaultProps = {
+  border: 'bottom',
+};
+
+function TableHeader({ border, children, mobileLabel }) {
   const isMobileLayout = useMobileLayout();
 
   const childrenMarkup = () => {
@@ -46,7 +71,7 @@ function TableHeader({ children, mobileLabel }) {
       role="row"
       width="100%"
       background="white"
-      border="bottom"
+      border={border}
       direction={isMobileLayout ? 'column' : 'row'}
       style={{ flexShrink: 0, minHeight: '50px' }}
       justify={isMobileLayout ? 'center' : 'start'}
@@ -58,5 +83,6 @@ function TableHeader({ children, mobileLabel }) {
 }
 
 TableHeader.propTypes = propTypes;
+TableHeader.defaultProps = defaultProps;
 
 export default TableHeader;
