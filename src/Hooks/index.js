24c1a3a2040ec1ffa2334a9f4494d75e59f4b850
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2019-08-08 14:59:03 -0700

    Hook: useResponsiveLayout
    returns an array where each element of the array corresponds to
    
    [isPhone, isTablet, isMedium, isLarge]

--- src/Hooks/index.js
+++ src/Hooks/index.js
@@ -2,4 +2,8 @@ export { default as useLockBodyScroll } from './UseLockBodyScroll';
 export { default as useKeyBoardEvent } from './UseKeyboardEvent';
 export { default as useMobileLayout } from './UseMobileLayout';
 export { default as useWindowSize } from './UseWindowSize';
+export {
+  default as useResponsiveLayout,
+} from './UseResponsiveLayout';
+
 export { default as useToast } from './UseToast';
