2b0e662df6db368f5977d34a322997064a3c6422
Nathan Young <nathanyoung@users.noreply.github.com>
2019-08-08 12:14:06 -0700

    useResponsiveLayout hook

--- src/Hooks/index.js
+++ src/Hooks/index.js
@@ -1,6 +1,9 @@
 export { default as useLockBodyScroll } from './UseLockBodyScroll';
 export { default as useKeyBoardEvent } from './UseKeyboardEvent';
 export { default as useMobileLayout } from './UseMobileLayout';
-export { default as usePhoneLayout } from './UsePhoneLayout';
 export { default as useWindowSize } from './UseWindowSize';
+export {
+  default as useResponsiveLayout,
+} from './UseResponsiveLayout';
+
 export { default as useToast } from './UseToast';
