3829ec8beee1a261acd9c3a0f2685c89cd1832dd
Nathan Young <nyoung@istreamplanet.com>
2019-09-17 16:30:45 -0700

    tests

--- src/Components/Metric/Metric.test.js
+++ src/Components/Metric/Metric.test.js
@@ -1,13 +1,129 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { useResponsiveLayout } from '../../Hooks';
 
+import Text from '../Text/Text';
+import Tooltip from '../Tooltip/Tooltip';
 import Metric from './Metric';
 
+jest.mock('../../Hooks/UseResponsiveLayout');
+
 describe('Metric', () => {
+  beforeEach(() => {
+    useResponsiveLayout.mockClear();
+    useResponsiveLayout.mockImplementation(() => [false, false]);
+  });
   it('renders without crashing', () => {
     expect(() => {
       shallow(<Metric value={54321} title="test" />);
     }).not.toThrow();
+  });
+
+  it('sets the correct Text sizes if not mobile', () => {
+    const wrapper = shallow(<Metric value="5" title="test metric" />);
+    expect(
+      wrapper
+        .find(Text)
+        .at(0)
+        .prop('size'),
+    ).toBe(6);
+    expect(
+      wrapper
+        .find(Text)
+        .at(1)
+        .prop('size'),
+    ).toBe(1);
+  });
+  it('sets the correct Text sizes if mobile', () => {
+    useResponsiveLayout.mockClear();
+    useResponsiveLayout.mockImplementation(() => [true, false]);
+    const wrapper = shallow(<Metric value="5" title="test metric" />);
+    expect(
+      wrapper
+        .find(Text)
+        .at(0)
+        .prop('size'),
+    ).toBe(7);
+    expect(
+      wrapper
+        .find(Text)
+        .at(1)
+        .prop('size'),
+    ).toBe(3);
+  });
+
+  describe('helpText', () => {
+    it('adds a Tooltip if helpText is set', () => {
+      const txt = 'tooltip help text';
+      const wrapper = shallow(
+        <Metric value="5" title="test metric" helpText={txt} />,
+      );
+      expect(wrapper.find(Tooltip)).toHaveLength(1);
+      expect(wrapper.find(Tooltip).prop('content')).toBe(txt);
+    });
+    it('does not render a Tooltip if helpText is not set', () => {
+      const wrapper = shallow(
+        <Metric value="5" title="test metric" />,
+      );
+      expect(wrapper.find(Tooltip)).toHaveLength(0);
+    });
+  });
+  describe('prefix and suffix', () => {
+    it('adds a prefix if set', () => {
+      const txt = 'prefix text';
+      const wrapper = shallow(
+        <Metric
+          value="5"
+          title="test metric"
+          prefix={txt}
+          prefixClassName="prefixClass"
+        />,
+      );
+      expect(wrapper.find('.prefixClass')).toHaveLength(1);
+      expect(wrapper.find('.prefixClass').html()).toContain(txt);
+    });
+    it('does not render a prefix if prefix is not set', () => {
+      const wrapper = shallow(
+        <Metric
+          value="5"
+          title="test metric"
+          prefixClassName="prefixClass"
+        />,
+      );
+      expect(wrapper.find('.prefixClass')).toHaveLength(0);
+    });
+    it('adds a suffix if set', () => {
+      const txt = 'suffix text';
+      const wrapper = shallow(
+        <Metric
+          value="5"
+          title="test metric"
+          suffix={txt}
+          suffixClassName="suffixClass"
+        />,
+      );
+      expect(wrapper.find('.suffixClass')).toHaveLength(1);
+      expect(wrapper.find('.suffixClass').html()).toContain(txt);
+    });
+    it('does not render a suffix if suffix is not set', () => {
+      const wrapper = shallow(
+        <Metric
+          value="5"
+          title="test metric"
+          suffixClassName="suffixClass"
+        />,
+      );
+      expect(wrapper.find('.suffixClass')).toHaveLength(0);
+    });
+  });
+
+  it('adds a Tooltip if helpText is set', () => {
+    const txt = 'tooltip help text';
+    const wrapper = shallow(
+      <Metric value="5" title="test metric" helpText={txt} />,
+    );
+    expect(wrapper.find(Tooltip)).toHaveLength(1);
+    expect(wrapper.find(Tooltip).prop('content')).toBe(txt);
   });
 
   describe('renderValue', () => {
