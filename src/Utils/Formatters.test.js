9a98e2ce77b7b4f725449aabc6acf078e77c6feb
Nathan Young <nyoung@istreamplanet.com>
2019-09-17 10:45:20 -0700

    Update Formatters.test.js

--- src/Utils/Formatters.test.js
+++ src/Utils/Formatters.test.js
@@ -8,5 +8,15 @@ describe('Util > Formatters', () => {
     it('returns value with commas separating thousands', () => {
       expect(Formatters.number(1234, { precision: 0 })).toBe('1,234');
     });
+    it('returns value with commas separating thousands and correct precision', () => {
+      expect(Formatters.number(1234.123456, { precision: 3 })).toBe(
+        '1,234.123',
+      );
+    });
+    it('returns value with commas separating thousands and correct precision', () => {
+      expect(Formatters.number(1234.19, { precision: 1 })).toBe(
+        '1,234.2',
+      );
+    });
   });
 });
