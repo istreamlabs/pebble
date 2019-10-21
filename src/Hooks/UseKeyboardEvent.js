8006fe9d3ef5b892220acb697a8a7f3be397acc0
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2019-10-21 14:06:29 -0700

    fix (UseKeyboardEvent): always preventDefault and add key modifier option for alt, ctrl, meta, shift (#224)
    fixes #223

--- src/Hooks/UseKeyboardEvent.js
+++ src/Hooks/UseKeyboardEvent.js
@@ -1,17 +1,42 @@
 import { useEffect } from 'react';
 
-export default (key, callback) => {
-  useEffect(() => {
-    const handler = function(event) {
-      if (event.key === key) {
-        callback();
-      }
-    };
+/**
+ * adds a keydown event listener to the window
+ * when the key(s) are pressed, it calls the callback
+ */
 
-    window.addEventListener('keydown', handler);
+export default (function(key, callback) {
+  useEffect(
+    function() {
+      const keys = key.split('+');
 
-    return () => {
-      window.removeEventListener('keydown', handler);
-    };
-  }, [key, callback]);
-};
+      const handler = function handler(event) {
+        const keyCommand = keys.every(k => {
+          switch (k.toLowerCase()) {
+            case 'alt':
+              return !!event.altKey;
+            case 'ctrl':
+              return !!event.ctrlKey;
+            case 'meta':
+              return !!event.metaKey;
+            case 'shift':
+              return !!event.shiftKey;
+            default:
+              return event.key === k;
+          }
+        });
+
+        if (keyCommand) {
+          event.preventDefault();
+          callback(event);
+        }
+      };
+
+      window.addEventListener('keydown', handler);
+      return function() {
+        window.removeEventListener('keydown', handler);
+      };
+    },
+    [key, callback],
+  );
+});
---
+++ src/Hooks/UseKeyboardEvent.test.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+
+import useKeyboardEvent from './UseKeyboardEvent';
+
+const TestComponent = ({ detectKeys, callback }) => {
+  useKeyboardEvent(detectKeys, callback);
+  return <div />;
+};
+
+TestComponent.propTypes = {
+  detectKeys: PropTypes.string,
+  callback: PropTypes.func,
+};
+
+afterEach(cleanup);
+
+describe('useKeyboardEvent', () => {
+  it('it calls the callback with the correct value when the keydown event is fired with a single key', async () => {
+    const callback = jest.fn();
+    const { container } = render(
+      <TestComponent detectKeys="s" callback={callback} />,
+    );
+    const keyDownEvent = new KeyboardEvent('keydown', {
+      key: 's',
+      bubbles: true,
+      which: 83,
+      code: 'KeyS',
+    });
+    fireEvent(container, keyDownEvent);
+    expect(callback).toHaveBeenCalledWith(keyDownEvent);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('it calls the callback with the correct value when the keydown event is fired with alt + letter', async () => {
+    const callback = jest.fn();
+    const { container } = render(
+      <TestComponent detectKeys="alt+s" callback={callback} />,
+    );
+    const keyDownEvent = new KeyboardEvent('keydown', {
+      key: 's',
+      bubbles: true,
+      which: 83,
+      code: 'KeyS',
+      altKey: true,
+    });
+    fireEvent(container, keyDownEvent);
+    expect(callback).toHaveBeenCalledWith(keyDownEvent);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('it calls the callback with the correct value when the keydown event is fired with ctrl + letter', async () => {
+    const callback = jest.fn();
+    const { container } = render(
+      <TestComponent detectKeys="ctrl+s" callback={callback} />,
+    );
+    const keyDownEvent = new KeyboardEvent('keydown', {
+      key: 's',
+      bubbles: true,
+      which: 83,
+      code: 'KeyS',
+      ctrlKey: true,
+    });
+    fireEvent(container, keyDownEvent);
+    expect(callback).toHaveBeenCalledWith(keyDownEvent);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('it calls the callback with the correct value when the keydown event but the string is all caps', async () => {
+    const callback = jest.fn();
+    const { container } = render(
+      <TestComponent detectKeys="CTRL+s" callback={callback} />,
+    );
+    const keyDownEvent = new KeyboardEvent('keydown', {
+      key: 's',
+      bubbles: true,
+      which: 83,
+      code: 'KeyS',
+      ctrlKey: true,
+    });
+    fireEvent(container, keyDownEvent);
+    expect(callback).toHaveBeenCalledWith(keyDownEvent);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('it calls the callback with the correct value when the keydown event is fired with meta + letter', async () => {
+    const callback = jest.fn();
+    const { container } = render(
+      <TestComponent detectKeys="meta+s" callback={callback} />,
+    );
+    const keyDownEvent = new KeyboardEvent('keydown', {
+      key: 's',
+      bubbles: true,
+      which: 83,
+      code: 'KeyS',
+      metaKey: true,
+    });
+    fireEvent(container, keyDownEvent);
+    expect(callback).toHaveBeenCalledWith(keyDownEvent);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+  it('it calls the callback with the correct value when the keydown event is fired with shift + letter', async () => {
+    const callback = jest.fn();
+    const { container } = render(
+      <TestComponent detectKeys="shift+s" callback={callback} />,
+    );
+    const keyDownEvent = new KeyboardEvent('keydown', {
+      key: 's',
+      bubbles: true,
+      which: 83,
+      code: 'KeyS',
+      shiftKey: true,
+    });
+    fireEvent(container, keyDownEvent);
+    expect(callback).toHaveBeenCalledWith(keyDownEvent);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+});
