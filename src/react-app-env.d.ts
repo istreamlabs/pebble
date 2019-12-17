ddac457725a5b7f7931f229dab1866bba7682a48
Erik Sjaastad <esjaastad@istreamplanet.com>
2019-12-17 11:39:51 -0800

    add env settings to react-app-env.d.ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+      readonly NODE_ENV: 'development' | 'production' | 'test';
+      readonly PUBLIC_URL: string;
+      REACT_APP_HASH: string
+      REACT_APP_API_URI: string
+      REACT_APP_WS_URI: string
+      }
+  }
+  interface Window {
+      Stripe: any
+  }
