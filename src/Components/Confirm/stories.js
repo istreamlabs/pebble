fa4b1eee838f33695af3383c0f6198e9e7ab9693
Eddie Monge Jr <eddiemonge@users.noreply.github.com>
2021-08-25 14:25:00 -0700

    feat: confirm now accepts a ReactDOM portal

--- src/Components/Confirm/stories.js
+++ src/Components/Confirm/stories.js
@@ -2,21 +2,41 @@ import '../../Styles/foundation.scss';
 
 import Block from '../Block/Block';
 import Confirm from './Confirm';
+import Card from '../Card/Card';
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 storiesOf('Confirm', module)
-  .add('basic', () => <Confirm>Confirm</Confirm>)
+  .add('basic', () => (
+    <Confirm onConfirm={action('Confirmed')}>Confirm</Confirm>
+  ))
   .add('Type a value', () => (
-    <Confirm confirmValue="confirm">Confirm</Confirm>
+    <Confirm confirmValue="confirm" onConfirm={action('Confirmed')}>
+      Confirm
+    </Confirm>
   ))
   .add('Danger', () => (
     <Block itemSpacing="3">
-      <Confirm danger primary icon="delete">
+      <Confirm
+        danger
+        primary
+        icon="delete"
+        onConfirm={action('Confirmed')}
+      >
         Destroy User
       </Confirm>
-      <Confirm danger iconAfterText="delete">
+      <Confirm
+        danger
+        iconAfterText="delete"
+        onConfirm={action('Confirmed')}
+      >
         Destroy
       </Confirm>
     </Block>
+  ))
+  .add('Portal', () => (
+    <Card sectioned>
+      <Confirm portalTarget={document.body}>Portal Me</Confirm>
+    </Card>
   ));
