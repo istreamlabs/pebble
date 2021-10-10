6ce6478c99a32f2436e932d502aa903b9d57c050
JD Smith <jdsmith2816@gmail.com>
2021-10-10 16:53:37 -0500

    feat: add ability to retain input after selection
    `FieldSelect` has a new `selectResetsInput` prop that controls whether
    the input value is reset upon selection.
    
    This defaults to true so as to avoid changing existing behavior.

--- src/Components/FieldSelect/stories.js
+++ src/Components/FieldSelect/stories.js
@@ -7,9 +7,16 @@ const options = [
   { value: 'blueberry', label: 'Blueberry' },
   { value: 'boysenberry', label: 'Boysenberry' },
   { value: 'bubblegum', label: 'Bubblegum' },
+  { value: 'cinnamon', label: 'Cinnamon' },
   { value: 'chocolate', label: 'Chocolate' },
   { value: 'green-tea', label: 'Green Tea' },
   { value: 'lychee', label: 'Lychee' },
+  { value: 'peach', label: 'Peach' },
+  { value: 'peppermint-fudge', label: 'Peppermint Fudge' },
+  { value: 'peppermint-stick', label: 'Peppermint Stick' },
+  { value: 'pineapple', label: 'Pineapple' },
+  { value: 'pineapple-sherbet', label: 'Pineapple Sherbet' },
+  { value: 'pistachio-nut', label: 'Pistachio Nut' },
   { value: 'strawberry', label: 'Strawberry' },
   { value: 'vanilla', label: 'Vanilla' },
 ];
@@ -63,6 +70,17 @@ function FieldSelectExamples() {
       />
 
       <FieldSelect
+        id="multi"
+        options={options}
+        multiSelect
+        className="mb-5"
+        isSearchable
+        label="Multi-Select: Selection Does Not Reset Input"
+        placeholder="choose one or many"
+        selectResetsInput={false}
+      />
+
+      <FieldSelect
         asyncSearch
         className="mb-5"
         id="multiSelectSearch"
@@ -82,6 +100,18 @@ function FieldSelectExamples() {
         isSearchable
         label="Checkbox Multi-Select"
         placeholder="choose one or many"
+      />
+
+      <FieldSelect
+        id="checkbox"
+        options={options}
+        showCheckbox
+        className="mb-5"
+        multiSelect
+        isSearchable
+        label="Checkbox Multi-Select: Selection Does Not Reset Input"
+        placeholder="choose one or many"
+        selectResetsInput={false}
       />
 
       <FieldSelect
